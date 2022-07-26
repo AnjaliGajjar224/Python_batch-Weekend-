f88c2305375fbb5c42e0f546680d23bff7558918
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-07-26 15:32:57 +0530

    Add files via upload

---
+++ 16thJuly.py
@@ -0,0 +1,121 @@
+"""
+Palindrom:
+
+121 - 121
+madam - madam
+# """
+# my_Str = input("Enter String:")
+
+# new_str = my_Str[::-1]
+
+# if my_Str == new_str:
+#     print("Palindrom")
+# else:
+#     print("NOt Palindrom")
+
+"""
+for loop:
+"""
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1,5):
+#     print(i)
+
+# for i in range(11,15):
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)
+
+# for i in range(5,0,-1):
+#     print(i)
+
+# for i in range(10,0,-2):
+#     print(i)
+
+# Str = "Hello"
+
+# for i in Str:
+#     print(i)
+
+# myList = [12,25.52,"Hello"]
+# print(myList)
+
+# for i in myList:
+#     print(type(i))
+#     print(i)
+
+"""
+1) 1 to n(user value)
+e.g. 1 2 3 4 5 
+
+2) n to 1
+e.g. 5 4 3 2 1 
+"""
+
+"""
+Nested For Loop
+"""
+# for i in range(5):
+#     for j in range(5):
+#         print("*", end="")
+#     print()
+
+"""
+54321
+4321
+321
+21
+1
+
+12345
+ 1234
+  123 
+   12
+    1
+"""
+
+"""
+for...else
+# """
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Loop is over")
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+# n = int(input("Enter number: "))
+# for i in range(n,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(1,6):
+#     k = 1
+#     for j in range(1,6):
+#         if j<=i:
+#             print(" ",end=" ")
+#         else:
+#             print(k,end=" ")
+#             k += 1
+# #     print()
+# sum = 0
+# for i in range(1,11):
+#     if i%2==0:
+#         sum = sum + i
+#     else:
+#         print("Odd number:",i)
+# print("Sum of even numbers:",sum)
+
+
+# for i in range(1,6):
+#     ch = 65
+#     for j in range(1,i+1):
+#         print(chr(ch),end=" ")
+#         ch += 1
+#     print()
